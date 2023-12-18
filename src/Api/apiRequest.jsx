3bc15634ca653be4285f9e18947913ad140443c1
Shukur939 <shukursurkhayli000@gmail.com>
2023-12-19 01:52:58 +0400

    antd

--- src/Api/apiRequest.jsx
+++ src/Api/apiRequest.jsx
@@ -3,3 +3,19 @@ import axios from "axios";
 const Api = axios.create({
     baseURL: "http://localhost:3005/users"
 })
+
+
+export const AddUsers = async (newUser) => {
+    try {
+        const response = await Api.post("/", newUser)
+        if (response.status !== 201) {
+            throw new Error("Error")
+        }
+        else {
+            return response.data
+        }
+    } catch (error) {
+        console.log(error.message);
+        throw error;
+    }
+}
--- src/Components/Add/index.jsx
+++ src/Components/Add/index.jsx
@@ -1,8 +1,34 @@
 import React from 'react'
+import Input from 'antd/es/input/Input'
 
 const Add = () => {
   return (
-    <div>index</div>
+    <>
+      <div className='text-center p-5 m-5'>
+        <h1>Add User</h1>
+        <div className='p-2'>
+        <Input 
+          name='fullName'
+          placeholder='enter fullName'
+          style={{
+            width:'50%',
+            borderRadius:"10px"
+          }}
+          />
+        </div>
+        <div className='p-2'>
+        <Input 
+          name='email'
+          placeholder='enter email'
+          style={{
+            width:'50%',
+            borderRadius:"10px"
+          }}
+          />
+        </div>
+
+      </div>
+    </>
   )
 }
 
